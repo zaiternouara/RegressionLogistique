03118247b64ca9be5c67070c2cdee94af6f48cee
ZAITERNouara <zaiternouara5@gmail.com>
2020-01-24 00:13:23 +0100

    Final

--- EquationNormal/EquationNormale.m
+++ EquationNormal/EquationNormale.m
@@ -1,4 +1,4 @@
-function  [theta ,vect_cost]  = EquationNormale(X,Y,lambda,n,NbIteration)
+function  [theta ,cost]  = EquationNormale(X,Y,lambda,n)
 
 Mat= eye(n);%renvoie une matrice d'identité n par n avec des éléments sur la diagonale principale et par des zéros le rest.
 t=zeros(n,1);%Matrice initialisé par zéro tel que le nombre n de ligne et avec une seul colonne
@@ -6,17 +6,8 @@ Matt=horzcat(t,Mat);%Concaténer deux vecteur
 
 Matt = [zeros(1,n+1) ; Matt];
 
- 
-
-
- 
-  for i = 1 : NbIteration
-     %La formule de l'equation Normale 
-    theta = inv(X'*X + lambda.*Matt )*(X'*Y);
-    
-    vect_cost(i,1) = Cost(X, Y , theta,lambda) ;
-    
-  end
+theta = inv(X'*X + lambda.*Matt )*(X'*Y);
+cost = Cost(X, Y , theta,lambda) ;
 
 
 
--- ent/Precision.m
+++
@@ -1,8 +0,0 @@
-function preci = Precision(X, Y, Theta)
-     
-    z = Theta * X';
-    [proba id]= max(Hypothesis_Sigmoid(z));
-   
-    preci = id' ;
-    preci = mean(double(Y == preci)*100);
-end
